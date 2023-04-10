037f26bd7f4d95b71b2b90611f746ace354cc60f
贝塔棋 <3188864257@qq.com>
2023-04-10 11:48:23 +0800

    📃 docs: Review

--- 03.medium/03-omit/template.ts
+++ 03.medium/03-omit/template.ts
@@ -1,3 +1,7 @@
 type MyOmit<T, K extends keyof T> = {
   [P in keyof T as P extends K ? never : P]: T[P];
 };
+
+// type MyOmit<T, K extends keyof T> = {
+//   [ k in keyof T as k extends K ? never : k ] : T[k]
+// }
--- 03.medium/08-medium-readonly-2/template.ts
+++ 03.medium/08-medium-readonly-2/template.ts
@@ -3,3 +3,13 @@ type MyReadonly2<T, K extends keyof T = keyof T> = {
 } & {
   [P in keyof T as P extends K ? never : P]: T[P];
 };
+
+// ================ 等价 ================
+
+// type MyReadonly<T, K extends keyof T = keyof T> = {
+//   [C in keyof T as C extends K ? never : C]: T[C]
+// }
+
+// type MyReadonly2<T, K extends keyof T = keyof T> = {
+//   readonly [P in K]: T[P]
+// } & MyReadonly<T, K>
